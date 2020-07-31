f980e887aac86e995432e8f5e50c5024bfad4848
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2020-07-31 16:15:04 -0400

    rename heroku files to demo

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: node app/dist/test/heroku_server.js
--- heroku_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-npm run tsc
-npm run wp
-cp -r build/. app/public/
-cp dist/test/heroku_server.js app/dist/test/heroku_server.js
-cp dist/src/server.js app/dist/src/server.js
-
