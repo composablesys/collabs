4acd1a8dc8115b58bbdc12998dcfde963d349f73
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2020-07-31 15:17:30 -0400

    fix gitignore

---
+++ app/dist/src/server.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.startBroadcastServer = void 0;
+const WebSocket = require("ws");
+/**
+ * This CRDT server will broadcast all messages that a client
+ * WebSocket broadcasting to every other connected WebSocket clients, excluding itself.
+ */
+function startBroadcastServer(webSocketArgs) {
+    // Initialize the WebSocket server instance.
+    const wss = new WebSocket.Server(webSocketArgs);
+    // Broacast function of the server.
+    wss.on('connection', function connection(ws) {
+        ws.on('message', function incoming(data) {
+            wss.clients.forEach(function each(client) {
+                if (client !== ws && client.readyState === WebSocket.OPEN) {
+                    client.send(data);
+                }
+            });
+        });
+    });
+}
+exports.startBroadcastServer = startBroadcastServer;
+//# sourceMappingURL=server.js.map
---
+++ app/dist/test/heroku_server.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const server_1 = require("../src/server");
+const express = require("express");
+// Express server based on https://devcenter.heroku.com/articles/node-websockets
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+const ROOT = __dirname + "../../../public";
+const server = express()
+    .use((req, res) => res.sendFile(req.path, { root: ROOT }))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+// Initialize the WebSocket server instance.
+//const wss = new WebSocket.Server({ port: 8080 });
+server_1.startBroadcastServer({ server });
+//# sourceMappingURL=heroku_server.js.map
