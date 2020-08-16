77791923695b954d78d1baa8255d0a4281b182c8
qhr961217-CMU <huairuiq@andrew.cmu.edu>
2020-08-16 05:11:05 -0400

    code format & add comments

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
-require('../test/test');// run test.ts
-
-// export default function Compoventuals(hi: string): void {
-
-// }
-
+require('../test/test'); // run test.ts
 import { CounterCrdt } from "../src/crdts/basic_crdts";
 import { CrdtNetworkRuntime } from '../src/network/crdt_network_runtime';
 
+/**
+ * Get Heroku server host Websocket.
+ */
 var HOST = location.origin.replace(/^http/, 'ws')
+
+/**
+ * Create CRDTs (e.g. CounterCrdt).
+ */
 let client = new CrdtNetworkRuntime("client", HOST);
 let clientCounter = new CounterCrdt("counterId", client);
 
+/* HTML variables */
 var counter = document.getElementById("counter");
 
+/* Customize onclick() function with CRDT operation */
 document.getElementById("increment")!.onclick = function() {
     console.log("clicked");
     clientCounter.add(100);
