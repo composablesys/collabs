d08da14425191e67706037e7d8bd77fd9842ce04
qhr961217-CMU <huairuiq@andrew.cmu.edu>
2020-08-16 20:02:39 -0400

    add negative number & demobuild

--- src/main.ts
+++ src/main.ts
@@ -16,9 +16,16 @@ let clientCounter = new CounterCrdt("counterId", client);
 /* HTML variables */
 var counter = document.getElementById("counter");
 
-/* Customize onclick() function with CRDT operation */
+/* Customize onclick() function of increment button with CRDT operation */
 document.getElementById("increment")!.onclick = function() {
-    console.log("clicked");
+    console.log("clicked increment");
     clientCounter.add(100);
+    counter!.innerHTML = clientCounter.value.toString();
+}
+
+/* Customize onclick() function of decrement button with CRDT operation */
+document.getElementById("decrement")!.onclick = function() {
+    console.log("clicked decrement");
+    clientCounter.add(-100);
     counter!.innerHTML = clientCounter.value.toString();
 }
