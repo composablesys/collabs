afdb5c0603416bbd5581505fd76516703eefaa69
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2020-11-13 10:49:32 -0500

    moved some tests

--- client/test/crdts/basic_crdts_tests.ts
+++ client/test/crdts/basic_crdts_tests.ts
@@ -1,20 +1,71 @@
 import assert from 'assert';
 import {TestingNetworkGenerator} from "../runtime_for_testing";
-import { Counter, AddEvent, MultEvent, MultRegister, GSet, MultiValueRegister, SetAddEvent, MvrEvent, LwwRegister, LwwEvent/*, GSet, MultiValueRegister*/ } from "../../src/crdts";
+import { Counter, AddEvent, MultEvent, MultRegister, GSet, MultiValueRegister, SetAddEvent, MvrEvent, LwwRegister, LwwEvent,/*, GSet, MultiValueRegister*/
+CounterNonResettable} from "../../src/crdts";
 
 let runtimeGen = new TestingNetworkGenerator();
 let alice = runtimeGen.newRuntime("alice");
 let bob = runtimeGen.newRuntime("bob");
 
-function testCounter() {
-    console.log("testCounter()...");
+function testCounterNonResettable() {
+    console.log("testCounterNonResettable()...");
 
-    let aliceCounter = new Counter(alice, "counterId");
+    let aliceCounter = new CounterNonResettable(alice, "counterId");
     aliceCounter.addEventListener("Add", event => console.log(
         "Alice: " + event.timestamp.getSender() + " added " + (event as AddEvent).valueAdded));
-    let bobCounter = new Counter(bob, "counterId");
+    let bobCounter = new CounterNonResettable(bob, "counterId");
     bobCounter.addEventListener("Add", event => console.log(
         "Bob: " + event.timestamp.getSender() + " added " + (event as AddEvent).valueAdded));
+    assert.strictEqual(aliceCounter.value, 0);
+    assert.strictEqual(bobCounter.value, 0);
+
+    aliceCounter.add(3);
+    runtimeGen.releaseAll();
+    assert.strictEqual(aliceCounter.value, 3);
+    assert.strictEqual(bobCounter.value, 3);
+
+    bobCounter.add(-4);
+    runtimeGen.releaseAll();
+    assert.strictEqual(aliceCounter.value, -1);
+    assert.strictEqual(bobCounter.value, -1);
+
+    aliceCounter.add(12);
+    runtimeGen.releaseAll();
+    assert.strictEqual(aliceCounter.value, 11);
+    assert.strictEqual(bobCounter.value, 11);
+
+    // Concurrent test
+    aliceCounter.add(2);
+    assert.strictEqual(aliceCounter.value, 13);
+    assert.strictEqual(bobCounter.value, 11);
+
+    bobCounter.add(-5);
+    assert.strictEqual(aliceCounter.value, 13);
+    assert.strictEqual(bobCounter.value, 6);
+
+    runtimeGen.releaseAll();
+    assert.strictEqual(aliceCounter.value, 8);
+    assert.strictEqual(bobCounter.value, 8);
+
+    console.log("...ok");
+}
+
+function testCounter() {
+    // Test Counter, including resets
+    console.log("testCounter()...");
+
+    let aliceCounter = new Counter(alice, "counterReset");
+    aliceCounter.addEventListener("Add", event => console.log(
+        "Alice: " + event.timestamp.getSender() + " added " + (event as AddEvent).valueAdded));
+    aliceCounter.addEventListener("Reset", event => console.log(
+        "Alice: " + event.timestamp.getSender() + " reset"
+    ));
+    let bobCounter = new Counter(bob, "counterReset");
+    bobCounter.addEventListener("Add", event => console.log(
+        "Bob: " + event.timestamp.getSender() + " added " + (event as AddEvent).valueAdded));
+    bobCounter.addEventListener("Reset", event => console.log(
+        "Bob: " + event.timestamp.getSender() + " reset"
+    ));
     assert.strictEqual(aliceCounter.value, 0);
     assert.strictEqual(bobCounter.value, 0);
 
@@ -46,6 +97,54 @@ function testCounter() {
     runtimeGen.releaseAll();
     assert.strictEqual(aliceCounter.value, 8);
     assert.strictEqual(bobCounter.value, 8);
+
+    // Observed reset tests
+    aliceCounter.reset();
+    runtimeGen.releaseAll();
+    assert.strictEqual(aliceCounter.value, 0);
+    assert.strictEqual(bobCounter.value, 0);
+
+    bobCounter.add(7);
+    runtimeGen.releaseAll();
+    assert.strictEqual(aliceCounter.value, 7);
+    assert.strictEqual(bobCounter.value, 7);
+
+    // Concurrent add should survive
+    aliceCounter.reset();
+    bobCounter.add(10);
+    runtimeGen.releaseAll();
+    assert.strictEqual(aliceCounter.value, 10);
+    assert.strictEqual(bobCounter.value, 10);
+
+    // Reset-wins tests
+    bobCounter.strongReset();
+    runtimeGen.releaseAll();
+    assert.strictEqual(aliceCounter.value, 0);
+    assert.strictEqual(bobCounter.value, 0);
+
+    aliceCounter.add(6);
+    runtimeGen.releaseAll();
+    assert.strictEqual(aliceCounter.value, 6);
+    assert.strictEqual(bobCounter.value, 6);
+
+    // Concurrent add should not survive
+    aliceCounter.strongReset();
+    bobCounter.add(20);
+    runtimeGen.releaseAll();
+    assert.strictEqual(aliceCounter.value, 0);
+    assert.strictEqual(bobCounter.value, 0);
+
+    // Lots of concurrency
+    aliceCounter.add(3);
+    bobCounter.add(7);
+    aliceCounter.reset();
+    runtimeGen.release(bob);
+    assert.strictEqual(aliceCounter.value, 7);
+    assert.strictEqual(bobCounter.value, 7);
+    bobCounter.strongReset();
+    runtimeGen.releaseAll();
+    assert.strictEqual(aliceCounter.value, 0);
+    assert.strictEqual(bobCounter.value, 0);
 
     console.log("...ok");
 }
@@ -281,6 +380,7 @@ function testLwwRegister() {
     console.log("...ok");
 }
 
+testCounterNonResettable();
 testCounter();
 testMultRegister();
 testGSet();
