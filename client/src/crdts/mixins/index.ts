67ff405681ec0988b0df1a8750213b3f3501a012
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2021-03-22 13:18:10 -0400

    client compiles

--- client/src/crdts/mixins/index.ts
+++ client/src/crdts/mixins/index.ts
@@ -1,6 +1,6 @@
 export * from "./abilities";
 // export * from "./AddAllAbilitiesViaChildren";
-export * from "./AddAllAbilitiesViaHistory";
+// export * from "./AddAllAbilitiesViaHistory";
 export * from "./mixin";
 export * from "./OutOfOrderAble";
 export * from "./Resettable";
