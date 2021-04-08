7f44d3d027b7097b7446050f3740affbf7e49bb8
RiaPradeep <ria.pradeep@gmail.com>
2021-04-08 00:10:17 -0700

    Updated JSON Crdt with single internal map

--- client/src/crdts/json.ts
+++ client/src/crdts/json.ts
@@ -1,152 +1,141 @@
-import {
-    Crdt,
-    CompositeCrdt,
-    CrdtEvent,
-    CrdtEventsRecord,
-  } from "./crdt_core";
+import { Crdt, CompositeCrdt, CrdtEvent, CrdtEventsRecord } from "./crdt_core";
 import { MultiValueRegister } from "./basic_crdts";
 import { MapCrdt } from "./standard";
 import { DefaultElementSerializer, ElementSerializer } from "./utils";
 
 export interface JsonEvent extends CrdtEvent {
-    readonly key: string;
-    readonly value: Crdt;
-  }
-
-export interface JsonEventsRecord extends CrdtEventsRecord {
-    Add: JsonEvent;
-    Change: JsonEvent;
-    Delete: JsonEvent;
-  }
-
-class JsonInternal {
-    private readonly internalNumberMap: MapCrdt<string, MultiValueRegister<Number>>;
-    private readonly internalStringMap: MapCrdt<string, MultiValueRegister<string>>;
-
-    constructor() {
-        let keySerializer : ElementSerializer<string> = DefaultElementSerializer.getInstance();
-        this.internalNumberMap = new MapCrdt(() => new MultiValueRegister(), keySerializer, true);
-        this.internalStringMap = new MapCrdt(() => new MultiValueRegister(), keySerializer, true);
-    }
-
-    set(key : string, val : any) {
-        switch (typeof val) {
-            case "string":
-                this.internalNumberMap.delete(key);
-                this.deleteSubKeys(key);
-                let mvr_str = this.internalStringMap.getForce(key);
-                mvr_str.value = val;
-                break;
-
-            case "number":
-                this.internalStringMap.delete(key);
-                this.deleteSubKeys(key);
-                let mvr_num = this.internalNumberMap.getForce(key);
-                mvr_num.value = val;
-                break;
-        }
-    }
-
-    get(key : string) : (number | string)[] {
-        let vals : any[] = [];
-        vals.push(this.internalStringMap.get(key)?.valueSet);
-        vals.push(this.internalNumberMap.get(key)?.valueSet);
-        return vals;
-    }
-
-    deleteSubKeys(key : string) {
-        for (let anyKey of this.internalStringMap.keys()) {
-            if (anyKey.includes(key + ":")) {
-                this.internalStringMap.delete(anyKey)
-            }
-        }
-
-        for (let anyKey of this.internalNumberMap.keys()) {
-            if (anyKey.includes(key + ":")) {
-                this.internalNumberMap.delete(anyKey)
-            }
-        }
-    }
-
-    delete(key : string) {
-        this.internalStringMap.delete(key);
-        this.internalNumberMap.delete(key);
-        this.deleteSubKeys(key);
-    }
-
-    keys(cursor : string) : string[] {
-        let keys = [];
-        
-        for (let anyKey of this.internalStringMap.keys()) {
-            keys.push(anyKey.substring(cursor.length));
-        }
-
-        for (let anyKey of this.internalNumberMap.keys()) {
-            keys.push(anyKey.substring(cursor.length));
-        }
-        
-        return keys;
-    }
-
-    values(cursor : string) : {values : (number | string)[], nestedObjs : Set<string>} {
-        let nested : Set<string> = new Set();
-        let vals : (number | string)[] = []
-
-        for (let key of this.keys(cursor)) {
-            if (key.includes(":")) {
-                nested.add(key.substr(0, key.indexOf(":")));
-            } else {
-                vals.push(...this.get(key));
-            }
-        }
-
-        return {values : vals, nestedObjs : nested};
-    }
-
-    hasKey(key : string) : boolean {
-        if (this.internalNumberMap.get(key) || this.internalStringMap.get(key)) {
-            return true;
-        }
-        return false;
-    }
+  readonly key: string;
+  readonly value: Crdt;
 }
 
-export class JsonCrdt extends CompositeCrdt<JsonEventsRecord> {
-    private internal : JsonInternal;
-    private cursor : string;
+export interface JsonEventsRecord extends CrdtEventsRecord {
+  Add: JsonEvent;
+  Change: JsonEvent;
+  Delete: JsonEvent;
+}
 
-    constructor(internal ?: JsonInternal, cursor ?: string) {
-        super();
+class JsonCrdt extends CompositeCrdt<JsonEventsRecord> {
+  private readonly internalMap: MapCrdt<
+    string,
+    MultiValueRegister<number | string | {}>
+  >;
 
-        if (!internal) internal = new JsonInternal();
-        this.internal = internal;
+  constructor() {
+    super();
 
-        if (!cursor) cursor = "";
-        this.cursor = cursor;
-    }
+    let keySerializer: ElementSerializer<string> = DefaultElementSerializer.getInstance();
+    this.internalMap = this.addChild(
+      "internalMap",
+      new MapCrdt(() => new MultiValueRegister(), keySerializer, true)
+    );
+  }
 
-    get(key : string) : (number | string)[] | JsonCrdt {
-        if (this.internal.hasKey(this.cursor + key)) {
-            return this.internal.get(this.cursor + key);
+  set(key: string, val: number | string | {}) {
+    this.internalMap.delete(key);
+    this.deleteSubKeys(key);
+    let mvr = this.internalMap.getForce(key);
+    mvr.value = val;
+  }
+
+  get(key: string): (number | string | JsonCursor)[] {
+    let vals: any[] = [];
+    let mvr = this.internalMap.get(key);
+    if (mvr) {
+      for (let val of mvr.valueSet) {
+        if (val == {}) {
+          vals.push(new JsonCursor(this, key));
         } else {
-            return new JsonCrdt(this.internal, this.cursor + ":" + key);
+          vals.push(val);
         }
+      }
     }
-    
-    set(key : string, val : any) { this.internal.set(this.cursor + key, val) }
+    return vals;
+  }
 
-    delete(key : string) { this.internal.delete(this.cursor + key) }
-
-    keys() : string[] { return this.internal.keys(this.cursor) }
-
-    values() : (number | string | JsonCrdt)[] {
-        let intermediateVals = this.internal.values(this.cursor);
-        let vals : (number | string | JsonCrdt)[] = intermediateVals.values;
-
-        for (let key of intermediateVals.nestedObjs) {
-            vals.push(new JsonCrdt(this.internal, this.cursor + ":" + key));
-        }
-
-        return vals;
+  deleteSubKeys(key: string) {
+    for (let anyKey of this.internalMap.keys()) {
+      if (anyKey.substring(0, key.length) == key) {
+        this.internalMap.delete(anyKey);
+      }
     }
+  }
+
+  delete(key: string) {
+    this.internalMap.delete(key);
+    this.deleteSubKeys(key);
+  }
+
+  keys(cursor: string): string[] {
+    let keys = [];
+
+    for (let anyKey of this.internalMap.keys()) {
+      if (anyKey.substring(0, cursor.length) == cursor)
+        keys.push(anyKey.substring(cursor.length));
+    }
+
+    return keys;
+  }
+
+  values(cursor: string): (number | string | JsonCursor)[] {
+    let vals: (number | string | JsonCursor)[] = [];
+
+    for (let key of this.keys(cursor)) {
+      vals.push(...this.get(key));
+    }
+
+    return vals;
+  }
+
+  hasKey(key: string): boolean {
+    return this.internalMap.has(key);
+  }
+
+  setIsMap(key: string) {
+    this.set(key, {});
+    this.deleteSubKeys(key);
+  }
+}
+
+export class JsonCursor {
+  private internal: JsonCrdt;
+  private cursor: string;
+
+  constructor(internal?: JsonCrdt, cursor?: string) {
+    if (!internal) internal = new JsonCrdt();
+    this.internal = internal;
+
+    if (!cursor) cursor = "";
+    this.cursor = cursor;
+  }
+
+  get(key: string): (number | string | JsonCursor)[] | JsonCursor {
+    if (this.internal.hasKey(this.cursor + key)) {
+      return this.internal.get(this.cursor + key);
+    } else {
+      return new JsonCursor(this.internal, this.cursor + ":" + key);
+    }
+  }
+
+  set(key: string, val: number | string) {
+    this.internal.set(this.cursor + key, val);
+  }
+
+  setMap(key: string) {
+    this.internal.setIsMap(key);
+  }
+
+  delete(key: string) {
+    this.internal.delete(this.cursor + key);
+  }
+
+  keys(): string[] {
+    return this.internal.keys(this.cursor);
+  }
+
+  values(): (number | string | JsonCursor)[] {
+    let intermediateVals = this.internal.values(this.cursor);
+    let vals: (number | string | JsonCursor)[] = intermediateVals;
+
+    return vals;
+  }
 }
