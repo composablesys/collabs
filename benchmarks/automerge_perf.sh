fa082f388f0a317be72c1a8f0271237e223650c8
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2021-04-28 19:48:15 -0400

    update scripts

--- benchmarks/automerge_perf.sh
+++ benchmarks/automerge_perf.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$3" ]
   then
-    echo "Usage: ./micro_yjs.sh <out folder> <warmup trials> <recorded trials>"
+    echo "Usage: ./automerge_perf.sh <out folder> <warmup trials> <recorded trials>"
     exit 1
 fi
 
@@ -16,9 +16,9 @@ for frequency in "whole" "rounds"
 do
     for measurement in "time" "network" "memory"
     do
-      for name in "Register" "LwwMap" "LwwMapRolling" "TextLtr" "TextRandom"
+      for name in "treedocLww" "treedocPrimitiveLww" "mapLww" "yjs" "automerge"
       do
-          npm start -- $1 $2 $3 "micro_yjs" $name $measurement $frequency
+          npm start -- $1 $2 $3 "automerge_perf" $name $measurement $frequency
       done
     done
 done
--- benchmarks/micro_automerge.sh
+++ benchmarks/micro_automerge.sh
@@ -1,34 +1,24 @@
 #/usr/bin/sh
 
-if [ -z "$1" ]
+if [ -z "$3" ]
   then
-    echo "Usage: ./run_all.sh <outdir>"
-    echo "(or ./run_all.sh --testRun to do a test run)"
+    echo "Usage: ./micro_automerge.sh <out folder> <warmup trials> <recorded trials>"
     exit 1
 fi
 
-if [ $1 == "--testRun" ]; then
+if [ $2 == "0" ] && [ $3 == "0" ]
+then
     echo "test run"
     set -e
 fi
 
-#git pull origin paper-benchmarks
-#cd ..
-#npm reset
-#npm run clean
-#npm install
-#npm run build
-#cd benchmarks
-
-
-# micro_crdts
-for measurement in "time" "network" "memory"
+for frequency in "whole" "rounds"
 do
-    for frequency in "whole" "rounds"
+    for measurement in "time" "network" "memory"
     do
         for name in "Register" "Counter" "CounterMap" "CounterMapRolling" "LwwMap" "LwwMapRolling" "TextLtr" "TextRandom"
         do
-            npm start -- $1 "micro_automerge" $name $measurement $frequency
+            npm start -- $1 $2 $3 "micro_automerge" $name $measurement $frequency
         done
     done
 done
