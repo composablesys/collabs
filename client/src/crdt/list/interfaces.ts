21a6813a52dea917dc56c3efd6679026d22110e0
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2021-06-22 19:02:53 -0400

    draft list interfaces

--- client/src/crdt/list/interfaces.ts
+++ client/src/crdt/list/interfaces.ts
@@ -1,177 +1,230 @@
-// TODO
-export interface CrdtList {}
+import { Crdt, CrdtEvent } from "../core";
+import { Resettable, ResettableEventsRecord } from "../helper_crdts";
 
-// // A list of opaque elements, supporting insert, delete,
-// // and move, with any semantics.
-// // TODO: way to access seqIds (e.g. expose the underlying
-// // binary tree).
-// // TODO: version of list that maps seqIds to themselves
-// export interface PlainList<T> extends Resettable {
-//   /** Alias for reset(). */
-//   clear(): void;
-//   // throws an error if out of bounds
-//   get(index: number): T;
-//   set(index: number, value: T): this;
-//   /**
-//    * Removes the last element from an array and returns it.
-//    * If the array is empty, undefined is returned and the array is not modified.
-//    */
-//   pop(): T | undefined;
-//   /**
-//    * Appends new elements to the end of an array, and returns the new length of the array.
-//    * @param items New elements to add to the array.
-//    */
-//   push(...items: T[]): number;
-//   /**
-//    * Inserts new elements at the start of an array, and returns the new length of the array.
-//    * @param items Elements to insert at the start of the array.
-//    */
-//   unshift(...items: T[]): number;
-//   /**
-//    * Removes the first element from an array and returns it.
-//    * If the array is empty, undefined is returned and the array is not modified.
-//    */
-//   shift(): T | undefined;
-//   /**
-//    * Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
-//    * @param start The zero-based location in the array from which to start removing elements.
-//    * @param deleteCount The number of elements to remove.
-//    * @returns An array containing the elements that were deleted.
-//    */
-//   splice(start: number, deleteCount?: number): T[];
-//   /**
-//    * Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
-//    * @param start The zero-based location in the array from which to start removing elements.
-//    * @param deleteCount The number of elements to remove.
-//    * @param items Elements to insert into the array in place of the deleted elements.
-//    * @returns An array containing the elements that were deleted.
-//    */
-//   splice(start: number, deleteCount: number, ...items: T[]): T[];
-//   // TODO: semantics: movement does an insertion (not
-//   // overwriting the existing toIndex), and toIndex is
-//   // done relative to the current state, not the state
-//   // after deleting fromIndex (which may shift toIndex's
-//   // meaning 1 to the right).  moves count elements,
-//   // works even if overlapping.
-//   move(fromIndex: number, toIndex: number, count?: number): this;
-//   // TODO
-//   // /**
-//   //  * Performs the specified action for each element in an array.
-//   //  * @param callbackfn  A function that accepts up to three arguments. forEach calls the callbackfn function one time for each element in the array.
-//   //  * @param thisArg  An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
-//   //  */
-//   // forEach(
-//   //   callbackfn: (value: T, index: number, array: T[]) => void,
-//   //   thisArg?: any
-//   // ): void;
-//   // /**
-//   //  * Reverses the elements in an array in place.
-//   //  * This method mutates the array and returns a reference to the same array.
-//   //  */
-//   // reverse(): T[];
-//
-//   readonly size: number;
-//
-//   /**
-//    * Returns an iterable of [index,entry] pairs for every entry in the list.
-//    */
-//   entries(): IterableIterator<[number, T]>;
-//
-//   /**
-//    * Returns an iterable of values in the list.
-//    */
-//   values(): IterableIterator<T>;
-//
-//   /** Alias for values(). */
-//   [Symbol.iterator](): IterableIterator<T>;
-//
-//   // TODO: include?  Same Q for other array inspection
-//   // methods that could run in sublinear time,
-//   // but will run in linear time if you copy this
-//   // to an array and then use the array method (e.g. some).
-//   /**
-//    * Returns a copy of a section of an array.
-//    * For both start and end, a negative index can be used to indicate an offset from the end of the array.
-//    * For example, -2 refers to the second to last element of the array.
-//    * @param start The beginning index of the specified portion of the array.
-//    * If start is undefined, then the slice begins at index 0.
-//    * @param end The end index of the specified portion of the array. This is exclusive of the element at the index 'end'.
-//    * If end is undefined, then the slice extends to the end of the array.
-//    */
-//   slice(start?: number, end?: number): T[];
-// }
-//
-// // TODO: remove fluency (redundant this return values),
-// // in case a subclass wants to overwrite them?
-//
-// // Not allowing revivals (Yjs semantics) lets you
-// // avoid making elements direct children; instead you can
-// // use a CrdtSet to hold the children, with arbitrary
-// // initializers, and the CrdtSet will id them by unique ids.
-// // That will make their names shorter (no seqId).
-// export interface CrdtList<C extends Crdt> extends Resettable {
-//   /** Alias for reset(). */
-//   clear(): void;
-//   // throws an error if out of bounds
-//   get(index: number): C;
-//   // TODO: create instead of insert?  For consistency
-//   // with set.  I find insert more intuitive, although
-//   // we would run into trouble if we later wanted
-//   // to add it to PList (instead of just having splice).
-//   insert(index: number): C;
-//   insertRange(index: number, count: number): C[];
-//   delete(index: number): void;
-//   deleteRange(index: number, count: number): void;
-//   // TODO: semantics: movement does an insertion (not
-//   // overwriting the existing toIndex), and toIndex is
-//   // done relative to the current state, not the state
-//   // after deleting fromIndex (which may shift toIndex's
-//   // meaning 1 to the right).  moves count elements,
-//   // works even if overlapping.
-//   move(fromIndex: number, toIndex: number, count?: number): this;
-//   // TODO
-//   // /**
-//   //  * Performs the specified action for each element in an array.
-//   //  * @param callbackfn  A function that accepts up to three arguments. forEach calls the callbackfn function one time for each element in the array.
-//   //  * @param thisArg  An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
-//   //  */
-//   // forEach(
-//   //   callbackfn: (value: T, index: number, array: T[]) => void,
-//   //   thisArg?: any
-//   // ): void;
-//   // /**
-//   //  * Reverses the elements in an array in place.
-//   //  * This method mutates the array and returns a reference to the same array.
-//   //  */
-//   // reverse(): T[];
-//
-//   readonly size: number;
-//
-//   /**
-//    * Returns an iterable of [index,entry] pairs for every entry in the list.
-//    */
-//   entries(): IterableIterator<[number, C]>;
-//
-//   /**
-//    * Returns an iterable of values in the list.
-//    */
-//   values(): IterableIterator<C>;
-//
-//   /** Alias for values(). */
-//   [Symbol.iterator](): IterableIterator<C>;
-//
-//   // TODO: include?  Same Q for other array inspection
-//   // methods that could run in sublinear time,
-//   // but will run in linear time if you copy this
-//   // to an array and then use the array method (e.g. some).
-//   /**
-//    * Returns a copy of a section of an array.
-//    * For both start and end, a negative index can be used to indicate an offset from the end of the array.
-//    * For example, -2 refers to the second to last element of the array.
-//    * @param start The beginning index of the specified portion of the array.
-//    * If start is undefined, then the slice begins at index 0.
-//    * @param end The end index of the specified portion of the array. This is exclusive of the element at the index 'end'.
-//    * If end is undefined, then the slice extends to the end of the array.
-//    */
-//   slice(start?: number, end?: number): C[];
-// }
+export interface ListEvent<V> extends CrdtEvent {
+  index: number;
+  value: V;
+}
+
+export interface ListIndexEvent extends CrdtEvent {
+  index: number;
+}
+
+export interface PlainListEventsRecord extends ResettableEventsRecord {
+  Insert: ListIndexEvent;
+  /**
+   * Index gives the former index (immediately before
+   * deleting).
+   */
+  Delete: ListIndexEvent;
+}
+
+export interface PlainList<
+  V,
+  Events extends PlainListEventsRecord = PlainListEventsRecord
+> extends Resettable<Events> {
+  /**
+   * Delete every index in this set, i.e.,
+   * deleteRange(0, this.length).
+   *
+   * Note that this may be a different semantics
+   * than reset.
+   */
+  clear(): void;
+  insert(index: number, value: V): this;
+  insertRange(startIndex: number, ...values: V[]): this;
+  // TODO: return the deleted value?  Could be weird if
+  // value is no longer usable.  Deleted boolean seems
+  // inappropriate because it will always be true.
+  delete(index: number): void;
+  /**
+   * Deletes range [fromIndex, toIndex).
+   * @param fromIndex [description]
+   * @param toIndex   [description]
+   */
+  deleteRange(fromIndex: number, toIndex: number): void;
+  // Not naming at() because there is an experimental
+  // Array.at that acts differently (e.g. allowing negative
+  // indices).
+  get(index: number): V;
+  // Optional?  Or non-supporting implementations just do
+  // delete-then-insert?
+  set(index: number, value: V): this;
+  /**
+   * TODO: include this?  If so: optional?  Default to
+   * delete & insert semantics?  Sub-interface?
+   *
+   * toIndex is evaluated before removing fromIndex
+   * and is used the same as the insertion index.
+   * So it'll end up at toIndex - 1 if fromIndex < toIndex,
+   * else toIndex.
+   * @param  fromIndex [description]
+   * @param  toIndex   [description]
+   * @return           [description]
+   */
+  move(fromIndex: number, toIndex: number): void;
+  readonly length: number;
+
+  // TODO: convenience aliases like push, etc.?  (Array has
+  // a bunch.)
+
+  /** Returns an iterable of values in the list. */
+  [Symbol.iterator](): IterableIterator<V>;
+  /** Returns an iterable of [index,entry] pairs for every entry in the list.
+   */
+  entries(): IterableIterator<[number, V]>;
+  /**
+   * Returns an iterable of values in the list.
+   */
+  values(): IterableIterator<V>;
+  /**
+   * Returns an iterable of indices in the list.
+   */
+  keys(): IterableIterator<number>;
+
+  // TODO: slice method (returning an array)?
+}
+
+/**
+ * Note that this doesn't extend CrdtEvent, because
+ * values may be initialized independently of
+ * message delivery, in which case there is
+ * no associated timestamp.
+ */
+export interface ListInitEvent<C extends Crdt> {
+  index: number;
+  value: C;
+}
+
+export interface CrdtListEventsRecord<C extends Crdt>
+  extends ResettableEventsRecord {
+  // TODO: also include value?  You can get it from the list
+  // though.
+  Insert: ListIndexEvent;
+  /**
+   * Index gives the former index (immediately before
+   * deleting).
+   */
+  Delete: ListIndexEvent;
+  /**
+   * Emitted when a valueCrdt is constructed.
+   * Use this to register event listeners on valueCrdts.
+   * Note that this may be called multiple times for the
+   * same conceptual value (if a valueCrdt is GC'd and then
+   * reconstructed), and it may not correspond precisely
+   * to Insert events.
+   */
+  ValueInit: ListInitEvent<C>;
+}
+
+export interface CrdtList<
+  C extends Crdt,
+  CreateArgs extends any[] = [],
+  Events extends CrdtListEventsRecord<C> = CrdtListEventsRecord<C>
+> extends Resettable<Events> {
+  insert(index: number, ...args: CreateArgs): C;
+  /**
+   * [restore description]
+   * @param  valueCrdt [description]
+   * @return the restored index.  TODO: remove this return
+   * value from restore in the sets/maps, for consistency.
+   */
+  restore(valueCrdt: C): number;
+  // TODO: return the deleted value?  Could be weird if
+  // value is no longer usable.  Deleted boolean seems
+  // inappropriate because it will always be true.
+  delete(index: number): void;
+  /**
+   * Deletes range [fromIndex, toIndex).
+   * @param fromIndex [description]
+   * @param toIndex   [description]
+   */
+  deleteRange(fromIndex: number, toIndex: number): void;
+  // TODO: convenience aliases like push, etc.?  (Array has
+  // a bunch.)
+
+  // Not naming at() because there is an experimental
+  // Array.at that acts differently (e.g. allowing negative
+  // indices).
+  get(index: number): C;
+  /**
+   * [indexOf description]
+   * @param  valueCrdt [description]
+   * @return undefined if this.has(valueCrdt) is false
+   */
+  indexOf(valueCrdt: C): number | undefined;
+  /**
+   * Returns true if valueCrdt is owned by this CrdtList,
+   * i.e., it resulted from an insert operation on this.
+   */
+  owns(valueCrdt: C): boolean;
+  has(valueCrdt: C): boolean;
+  /**
+   * Delete every value in this list, i.e.,
+   * deleteRange(0, this.length).
+   *
+   * Note that this may be a different semantics
+   * than reset.
+   */
+  clear(): void;
+  // TODO: size instead?
+  readonly length: number;
+
+  /** Returns an iterable of value Crdts in the list. */
+  [Symbol.iterator](): IterableIterator<C>;
+  /** Returns an iterable of [index,entry] pairs for every entry in the list.
+   */
+  entries(): IterableIterator<[number, C]>;
+  /**
+   * Returns an iterable of value Crdts in the list.
+   */
+  values(): IterableIterator<C>;
+  /**
+   * Returns an iterable of indices in the list.
+   */
+  keys(): IterableIterator<number>;
+
+  // TODO: slice method (returning an array)?
+}
+
+// TODO: include value?
+export interface ListMoveEvent extends CrdtEvent {
+  /**
+   * The index where it started on this replica.
+   * Will equal fromIndex on the sender, but maybe
+   * not on other replicas.
+   */
+  oldIndex: number;
+  /**
+   * The index where it ended (is currently).
+   * Might not equal toIndex even on the sender,
+   * in case fromIndex < toIndex (then it is toIndex - 1).
+   */
+  newIndex: number;
+}
+
+export interface CrdtMovableListEventsRecord<C extends Crdt>
+  extends CrdtListEventsRecord<C> {
+  Move: ListMoveEvent;
+}
+
+// TODO: CrdtMovableList or MovableCrdtList?
+// Or, separate Movable interface that works for both
+// (not extending anything)?
+export interface CrdtMovableList<
+  C extends Crdt,
+  CreateArgs extends any[] = [],
+  Events extends CrdtMovableListEventsRecord<C> = CrdtMovableListEventsRecord<C>
+> extends CrdtList<C, CreateArgs, Events> {
+  /**
+   * toIndex is evaluated before removing fromIndex
+   * and is used the same as the insertion index.
+   * So it'll end up at toIndex - 1 if fromIndex < toIndex,
+   * else toIndex.
+   */
+  move(fromIndex: number, toIndex: number): void;
+  // TODO: moveValue?  Usually, can just use indexOf to
+  // get a fromIndex; but it could be useful as a combined
+  // restore-then-move op, or for weird cases like dangling
+  // values.
+}
