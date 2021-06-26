82cd5b7f854362243b6a545c486113f209d00c23
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2021-06-25 20:30:40 -0400

    add matrix_widget demo template

--- demo/webpack.config.ts
+++ demo/webpack.config.ts
@@ -11,6 +11,7 @@ const config: webpack.Configuration = {
     counter_webrtc: "./src/site/counter_webrtc.ts",
     minesweeper: "./src/site/minesweeper.ts",
     aspace: "./src/site/aspace.ts",
+    matrix_widget: "./src/site/matrix_widget.ts",
     // TODO: Add more entries as needed for new pages
   },
   output: {
@@ -51,6 +52,7 @@ const config: webpack.Configuration = {
         { from: "./src/site/counter_webrtc.html", to: "counter_webrtc.html" },
         { from: "./src/site/minesweeper.html", to: "minesweeper.html" },
         { from: "./src/site/aspace.html", to: "aspace.html" },
+        { from: "./src/site/matrix_widget.html", to: "matrix_widget.html" },
         // TODO: Add more entries as needed for new pages
       ],
     }),
