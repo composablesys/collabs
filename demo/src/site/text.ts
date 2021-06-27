790f4a6be9acebb6615cb83be9c3354591fe97a8
BenGeordie <school@bengeordie@gmail.com>
2021-06-27 13:34:35 -0400

    text.ts overhaul. yata.ts cleanup

--- demo/src/site/text.ts
+++ demo/src/site/text.ts
@@ -9,7 +9,7 @@ let client = new Runtime(
     new DefaultCausalBroadcastNetwork(
         new WebSocketNetwork(HOST, "variable_counter")
     )
-    // , {periodMs: 5000}
+    , {periodMs: 5000}
 );
 
 let clientText = client.registerCrdt("text", new YataLinear<string>(''));
@@ -22,80 +22,118 @@ interface IDelta {
     ops: DeltaOperation[]
 }
 
-const getIdx = (delta: IDelta): {idx: number, insert?: any, delete?: any} => {
+const getRelevantDeltaOperation = (delta: IDelta): {
+    idx: number, insert?: string, delete?: number, attributes?: Record<string, any>, retain?: number
+} => {
     let idx = 0;
     for (let op of delta.ops) {
-        if (op.retain) {
+        if (op.retain && !op.attributes) {
             idx += op.retain;
-        }
-        if (op.insert || op.delete) {
+        } else {
             return {idx, ...op};
         }
     }
     return {idx: 0};
 }
 
-quill.on('text-change', function(delta, oldDelta, source) {
+quill.on("text-change", (delta, oldDelta, source) => {
     if (source === "user") {
-        const idxObj = getIdx(delta);
-        if (idxObj.insert) {
-            clientText.insertByIdx(client.replicaId, idxObj.idx, idxObj.insert);
-        } else if (idxObj.delete) {
-            clientText.deleteByIdx(idxObj.idx);
+        const op = getRelevantDeltaOperation(delta);
+        // Insertion (always one character)
+        if (op.insert) {
+            clientText.insertByIdx(
+                client.replicaId, client.getReplicaUniqueNumber(), op.idx, op.insert, op.attributes);
+        }
+        // Deletion (can be many characters)
+        else if (op.delete) {
+            clientText.deleteByIdx(
+                client.replicaId, client.getReplicaUniqueNumber(), op.idx, op.delete);
+        }
+        // Formatting (can be many characters)
+        else if (op.attributes && op.retain) {
+            clientText.changeAttributeByIdx(
+                client.replicaId, client.getReplicaUniqueNumber(), op.idx, op.retain, op.attributes);
         }
     }
-});
-
-// let idRightOfCursor = clientText.END;
-
-quill.on('selection-change', function(range, oldRange, source) {
-    if (source === 'user') {
-        // idRightOfCursor = clientText.getIdAtIdx(range.index);
-    }
 })
 
-// document.getElementById("text_button")!.onclick = function () {
-//     const idx = parseFloat(document.getElementById('text_index')!.nodeValue!);
-//     const char = document.getElementById('text_character')!.nodeValue!;
-//     clientText.insert(
-//         client.getReplicaId(),
-//         client.getReplicaUniqueNumber(),
-//         clientText.toIdArray()[idx],
-//         clientText.toIdArray()[idx + 1],
-//         char
-//     )
-// };
-
-// const updateText = () => {
-//     console.log(clientText.toArray());
-//     document.getElementById('text')!.innerHTML = clientText.toArray().join('');
+// const getIdx = (delta: IDelta): {idx: number, insert?: any, delete?: any, attributes?: Record<string, any>, retain?: any} => {
+//     let idx = 0;
+//     for (let op of delta.ops) {
+//         if (op.retain && !op.attributes) {
+//             idx += op.retain;
+//         }
+//         if (op.insert || op.delete || op.attributes) {
+//             return {idx, ...op};
+//         }
+//     }
+//     return {idx: 0};
 // }
-
-clientText.opMap.on("Add", (_) => {
-    console.log("Add event!")
-    const rightId = _.value.rightId;
-    const leftId = _.value.leftId;
-    const id = clientText.opMap.uidOf(_.value);
-    clientText.op(rightId).leftId = id;
-    clientText.op(leftId).rightId = id;
-})
-
-clientText.on("Change", (e) => {
-    console.log("Change event!", e);
-    const str = clientText.toArray().join('');
-    quill.insertText(0, str, "api");
-    quill.deleteText(str.length, quill.getLength() - 1);
-
-    // const len = quill.getLength();
-    // console.log("quill length is", len);
-    // console.log("str length is", str.length);
-    // console.log("About to set selection to correspond with id:", idRightOfCursor);
-    // quill.setSelection({index: clientText.getIdxOfId(idRightOfCursor), length: 0});
-    // quill.setSelection(range);
-
-
-    // // quill.focus();
-    // // quill.setSelection(0,1, "user");
-    // console.log("cursor idx", clientText.getCursorIdx());
-    // quill.setSelection({index: clientText.getCursorIdx(), length:0})
-});
+//
+// let idLeftOfCursor = clientText.START;
+//
+// quill.on('text-change', function(delta, oldDelta, source) {
+//     if (source === "user") {
+//         console.log("TEXT CHANGE FROM USER")
+//         const idxObj = getIdx(delta);
+//         if (idxObj.insert) {
+//             idLeftOfCursor = clientText.insertByIdx(client.replicaId, idxObj.idx, idxObj.insert.split(''), idxObj.attributes);
+//             console.log("text-change: insert: idLeftOfCursor", [idLeftOfCursor], "idx:", clientText.getIdxOfId(idLeftOfCursor));
+//         } else if (idxObj.delete) {
+//             idLeftOfCursor = clientText.deleteByIdx(idxObj.idx, idxObj.delete);
+//             console.log("text-change: delete: idLeftOfCursor", [idLeftOfCursor], "idx:", clientText.getIdxOfId(idLeftOfCursor));
+//         } else if (idxObj.attributes) {
+//             idLeftOfCursor = clientText.changeAttributeByIdx(idxObj.idx, idxObj.retain, idxObj.attributes);
+//             console.log("text-change: attribute: idLeftOfCursor", [idLeftOfCursor], "idx:", clientText.getIdxOfId(idLeftOfCursor));
+//         }
+//
+//     }
+// });
+//
+// quill.on('selection-change', function(range, oldRange, source) {
+//     console.log('selection-change from', source);
+//     idLeftOfCursor = clientText.getIdAtIdx((range ? range.index : oldRange.index) - 1);
+//     console.log("selection-change: idLeftOfCursor = ", [idLeftOfCursor], "idx = ", clientText.getIdxOfId(idLeftOfCursor));
+//     if (source === 'user') {
+//         // console.log("selection-change", range, oldRange, source);
+//         // console.log("selection-change: idLeftOfCursor = ", [idLeftOfCursor], "idx = ", clientText.getIdxOfId(idLeftOfCursor));
+//     }
+// })
+//
+// clientText.opMap.on("Add", (_) => {
+//     console.log("Add event!")
+//     const rightId = _.value.rightId;
+//     const leftId = _.value.leftId;
+//     const id = clientText.opMap.uidOf(_.value);
+//     clientText.op(rightId).leftId = id;
+//     clientText.op(leftId).rightId = id;
+// })
+//
+// clientText.on("Change", (e) => {
+//     console.log("change event", e);
+//     console.log("quill length", quill.getLength());
+//     console.log("quill content", quill.getText());
+//     const deleteLength = quill.getLength() - 1;
+//     // const str = clientText.toArray().join('');
+//     const textArr = clientText.toArray();
+//     console.log("on Change client text to array", textArr);
+//     console.log("on Change client text to id array", clientText.toIdArray());
+//     for (let i = textArr.length - 1; i >= 0; i--) {
+//         console.log(deleteLength);
+//         quill.deleteText(0, Math.min(deleteLength, i + 1));
+//         console.log("deleted", Math.min(deleteLength, i + 1), "characters");
+//         quill.insertText(0, "a".repeat(i) + textArr[i].content, textArr[i].attributes);
+//         console.log("inserted", "a".repeat(i) + textArr[i].content);
+//     }
+//     // textArr.forEach((elem, index) => {
+//     //     console.log("foreach", index, elem.content, elem.attributes);
+//     //     quill.insertText(index, elem.content, elem.attributes);
+//     // });
+//     // quill.insertText(0, str, "api");
+//     console.log("idLeftOfCursor is", [idLeftOfCursor]);
+//     console.log("(idx of idLeftOfCursor) + 1 is", clientText.getIdxOfId(idLeftOfCursor) + 1)
+//     quill.setSelection({index: clientText.getIdxOfId(idLeftOfCursor) + 1, length: 0}, 'api');
+//     // quill.deleteText(textArr.length, deleteLength);
+//     console.log(quill.getContents());
+//
+// });
--- demo/src/site/yata.ts
+++ demo/src/site/yata.ts
@@ -1,20 +1,16 @@
 import * as crdts from 'compoventuals-client';
+import {Crdt, CrdtEvent, CrdtEventsRecord, PrimitiveCrdt, ResettableEventsRecord} from "compoventuals-client";
 
 class YataLinearOp<T> extends crdts.CompositeCrdt {
-    readonly _creatorId: string; // Fixed
-    readonly _originId: string; // Fixed
+    readonly _creatorId: string;
+    readonly _originId: string;
     public _leftId: string;
     public _rightId: string;
     readonly _deleted: crdts.LwwRegister<boolean>;
-    // TODO: consider: deleted should store the client id of the last delete.
-    // TODO: consider: add a "causes" field to facilitate concurrently
-    //  performing sets of causal operations. For example, we can
-    //  model "moves" as a deletion which then causes an insertion.
-    //  We can prevent duplication from concurrent moving operations
-    //  if only one deletion is kept and anything caused by the
-    //  discarded deletion is also discarded.
-    readonly _content: T; // Fixed
-    readonly _pos: number; // Fixed
+    readonly _content: T;
+    readonly _pos: number;
+    readonly attributes: crdts.LwwPlainMap<string, any>;
+    private readonly attributesArg?: IterableIterator<[string, any]> | [string, any][];
 
     get rightId (): string {
         return this._rightId;
@@ -63,6 +59,7 @@ class YataLinearOp<T> extends crdts.CompositeCrdt {
         rightId: string,
         content: T,
         pos: number,
+        attributes?: [string, any][],
     ) {
         super();
         this._creatorId = creatorId;
@@ -72,10 +69,18 @@ class YataLinearOp<T> extends crdts.CompositeCrdt {
         this._deleted = this.addChild("deleted", new crdts.LwwRegister(false));
         this._content = content;
         this._pos = pos;
+        this.attributes = this.addChild("attributes",
+            new crdts.LwwPlainMap());
+        if (attributes) {
+            this.attributesArg = attributes;
+        }
     }
 
     init(name: string, parent: crdts.CrdtParent) {
         super.init(name, parent);
+        for (let entry in this.attributesArg) {
+            this.attributes.set(entry[0], entry[1]);
+        }
     }
 }
 
@@ -83,7 +88,7 @@ export class YataLinear<T> extends crdts.CompositeCrdt {
     private start: string = '';
     private end: string = '';
     readonly defaultContent: T;
-    public opMap: crdts.YjsCrdtSet<YataLinearOp<T>, [string, string, string, T]>;
+    public opMap: crdts.YjsCrdtSet<YataLinearOp<T>, [string, string, string, T, [string, any][]?]>;
     private idRightOfCursor: string = '';
     private startOp: YataLinearOp<T>;
     private endOp: YataLinearOp<T>;
@@ -102,10 +107,12 @@ export class YataLinear<T> extends crdts.CompositeCrdt {
                 leftIntent: string,
                 rightIntent: string,
                 content: T,
+                attributeEntries?: [string, any][]
             ) => {
                 const originId = leftIntent;
                 let leftId = leftIntent;
                 while (this.op(leftId).rightId !== rightIntent) {
+                    console.log("wat dis? left id is", leftId);
                     const o_id = this.op(leftId).rightId;
                     const o = this.op(this.op(leftId).rightId);
                     const i_origin = this.op(originId);
@@ -117,9 +124,11 @@ export class YataLinear<T> extends crdts.CompositeCrdt {
                         (o.originId !== originId
                             || o.creatorId < replicaId)
                     ) {
+                        console.log("Compare leftId:", leftId, "and o_id:", o_id);
                         leftId = o_id;
                     } else {
-                        if (i_origin.pos > o_origin.pos) {
+                        console.log("i_origin.pos =", i_origin.pos, "o_origin.pos", o_origin.pos);
+                        if (i_origin.pos >= o_origin.pos) {
                             break;
                         }
                     }
@@ -129,11 +138,77 @@ export class YataLinear<T> extends crdts.CompositeCrdt {
                 // this.op(leftId).rightId = id;
                 // this.op(rightId).leftId = id;
                 const pos = (this.op(leftId).pos + this.op(rightId).pos) / 2;
-                return new YataLinearOp<T>(replicaId, originId, leftId, rightId, content, pos)
+                // const finalAttrEntries = attributeEntries ? attributeEntries : Array.from(this.op(leftId).attributes.entries());
+                // return new YataLinearOp<T>(replicaId, originId, leftId, rightId, content, pos);
+                return new YataLinearOp<T>(replicaId, originId, leftId, rightId, content, pos, attributeEntries);
             }, [
                 this.startOp,
                 this.endOp
             ]));
+    }
+
+    init(name: string, parent: crdts.CrdtParent) {
+        super.init(name, parent);
+        this.START = this.opMap.uidOf(this.startOp);
+        this.END = this.opMap.uidOf(this.endOp);
+        console.log(`START: ${this.START} | END: ${this.END}`);
+        this.opMap.getByUid(this.START)!.rightId = this.END;
+        this.opMap.getByUid(this.END)!.leftId = this.START;
+        this.idRightOfCursor = this.END;
+    }
+
+    private insert (
+        replicaId: string,
+        leftIntent: string,
+        rightIntent: string,
+        content: T,
+        attributeEntries?: [string, any][]
+    ): string {
+        return this.opMap.uidOf(this.opMap.create(replicaId, leftIntent, rightIntent, content, attributeEntries));
+    }
+
+    private delete (id: string): void {
+        this.op(id).delete();
+    }
+
+    private changeAttributes (id: string, attributes: Record<string, any>): void {
+        for (const attr in attributes) {
+            console.log("About to change", attr, "to", attributes[attr]);
+            this.op(id).attributes.set(attr, attributes[attr]);
+        }
+        // let entry = attributes.next();
+        // while (!entry.done) {
+        //     this.op(id).attributes[entry[0]].value = entry[1].value;
+        //     entry = attributes.next();
+        // }
+    }
+
+    private toArrayNode (nodeId: string): {content:T, attributes: Record<string, any>}[] {
+        const node = this.op(nodeId)
+        if (node.pos === 0) return [];
+        const arr = this.toArrayNode(node.leftId);
+        if (!node.deleted) {
+            console.log("toArrayNode", node.content, Object.fromEntries(node.attributes.entries()));
+            arr.push({
+                content: node.content,
+                attributes: Object.fromEntries(node.attributes.entries())
+            });
+        }
+        return arr;
+    }
+
+    private toIdArrayNode (nodeId: string): string[] {
+        const node = this.op(nodeId)
+        if (node.pos === 1) return [this.END];
+        const array = this.toIdArrayNode(node.rightId)
+        if (!node.deleted) {
+            array.unshift(nodeId);
+        }
+        return array;
+    }
+
+    toIdArray (): string[] {
+        return this.toIdArrayNode(this.START);
     }
 
     op(id:string): YataLinearOp<T> {
@@ -160,87 +235,63 @@ export class YataLinear<T> extends crdts.CompositeCrdt {
         this.end = e;
     }
 
-    init(name: string, parent: crdts.CrdtParent) {
-        super.init(name, parent);
-        this.START = this.opMap.uidOf(this.startOp);
-        this.END = this.opMap.uidOf(this.endOp);
-        console.log(`START: ${this.START} | END: ${this.END}`);
-        this.opMap.getByUid(this.START)!.rightId = this.END;
-        this.opMap.getByUid(this.END)!.leftId = this.START;
-        this.idRightOfCursor = this.END;
-    }
-
-    insert (
-        replicaId: string,
-        leftIntent: string,
-        rightIntent: string,
-        content: T,
-    ): string {
-        return this.opMap.uidOf(this.opMap.create(replicaId, leftIntent, rightIntent, content));
-    }
-
-    delete (id: string): void {
-        console.log("deleted id:", id);
-        this.op(id).delete();
-    }
-
-    private toArrayNode (nodeId: string): T[] {
-        const node = this.op(nodeId)
-        if (node.pos === 0) return [];
-        const arr = this.toArrayNode(node.leftId);
-        if (!node.deleted) {
-            arr.push(node.content);
-        }
-        return arr;
-    }
-    
-    toArray (): T[] {
+    toArray (): {content:T, attributes: Record<string, any>}[] {
         return this.toArrayNode(this.op(this.END).leftId);
     }
 
-    private toIdArrayNode (nodeId: string): string[] {
-        const node = this.op(nodeId)
-        if (node.pos === 1) return [this.END];
-        const array = this.toIdArrayNode(node.rightId)
-        if (!node.deleted) {
-            array.unshift(nodeId);
-        }
-        return array;
-    }
-
-    private toIdArray (): string[] {
-        console.log("To Id Array");
-        return this.toIdArrayNode(this.START);
-    }
-
     getIdAtIdx (idx: number): string {
-        console.log("Get Id At Idx");
         return this.toIdArray()[idx + 1];
     }
 
     insertByIdx (
         replicaId: string,
+        replicaUniqueNumber: number,
         idx: number,
         content: T,
+        attributeObj?: Record<string, any>
     ): void {
-        const leftIntentId = this.getIdAtIdx(idx - 1);
-        this.idRightOfCursor = this.op(
-            this.insert(
-                replicaId,
-                leftIntentId,
-                this.op(leftIntentId).rightId,
-                content,
-            )
-        ).rightId;
+        let idLeftOfCursor = this.getIdAtIdx(idx - 1);
+        this.insert(
+            replicaId,
+            idLeftOfCursor,
+            this.op(idLeftOfCursor).rightId,
+            content,
+            attributeObj ? Object.entries(attributeObj) : undefined
+        );
     }
 
     deleteByIdx (
+        replicaId: string,
+        replicaUniqueNumber: number,
         idx: number,
+        len: number,
     ): void {
-        const id = this.getIdAtIdx(idx);
+        const id1 = this.getIdAtIdx(idx);
+        const id2 = this.getIdAtIdx(idx + len);
+        let id = id1;
+        while (id !== id2) {
+            this.delete(id);
+            id = this.op(id).rightId;
+        }
         this.delete(id);
-        this.idRightOfCursor = this.op(id).rightId;
     }
+
+    changeAttributeByIdx(
+        replicaId: string,
+        replicaUniqueNumber: number,
+        idx: number,
+        len: number,
+        attributes: Record<string, any>
+    ): void {
+        console.log("Change attribute by idx", idx, len, attributes);
+        const id1 = this.getIdAtIdx(idx);
+        const id2 = this.getIdAtIdx(idx + len);
+        let id = id1;
+        while (id !== id2) {
+            this.changeAttributes(id, attributes);
+            id = this.op(id).rightId;
+        }
+    };
 
     getIdxOfId (id: string): number {
         if (id === this.START) return -1;
@@ -248,11 +299,8 @@ export class YataLinear<T> extends crdts.CompositeCrdt {
         if (this.op(id).deleted) return prev;
         return 1 + prev;
     }
-
-    getCursorIdx (): number {
-        return this.getIdxOfId(this.idRightOfCursor);
-    }
-
-    // TODO: Make composable so the type can be CRDT of T or T?
-    // TODO: Then also make a fromArray.
 }
+
+// TODO:
+//  1. Link text-change event with attribute changes. DONE
+//  2. Make a toArray function that also includes attributes
