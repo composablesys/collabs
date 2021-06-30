6c85b33e440f71c6dd5a6e4aa50b19142b4c7c51
RiaPradeep <ria.pradeep@gmail.com>
2021-06-30 15:36:30 -0700

    Add draft of multiple semidirect product

---
+++ client/src/crdt/helper_crdts/multiple_semidirect_product.ts
@@ -0,0 +1,356 @@
+import { CausalTimestamp } from "../../net";
+import { Crdt, CrdtEventsRecord, CrdtParent, StatefulCrdt } from "../core";
+import { LocallyResettableState } from "./resettable";
+
+class StoredMessage {
+  constructor(
+    readonly senderCounter: number,
+    readonly receiptCounter: number,
+    readonly targetPath: string[],
+    readonly timestamp: CausalTimestamp | null,
+    readonly message: Uint8Array,
+    readonly arbIndex: number // arbitration number
+  ) {}
+}
+
+class MultipleSemidirectStateBase<S extends Object> {
+  protected receiptCounter = 0;
+  // H maps sender -> history of messages from that replica
+  // Maybe instead, i -> history of messages from that CRDT
+  protected history: Map<string, Array<StoredMessage>> = new Map();
+  public internalState!: S;
+  constructor(private readonly historyTimestamps: boolean) {}
+
+  /**
+   * Add message to the history with the given timestamp.
+   * replicaId is our replica id.
+   */
+  add(
+    replicaId: string,
+    targetPath: string[],
+    timestamp: CausalTimestamp,
+    message: Uint8Array,
+    arbId: number
+  ) {
+    let senderHistory = this.history.get(timestamp.getSender());
+    if (senderHistory === undefined) {
+      senderHistory = [];
+      this.history.set(timestamp.getSender(), senderHistory);
+    }
+    senderHistory.push(
+      new StoredMessage(
+        timestamp.getSenderCounter(),
+        this.receiptCounter,
+        targetPath,
+        this.historyTimestamps ? timestamp : null,
+        message,
+        arbId
+      )
+    );
+    this.receiptCounter++;
+  }
+
+  /**
+   * Return all messages in the history concurrent to the given
+   * timestamp, in some causal order (specifically, this replica's
+   * receipt order).  If we are the sender (i.e., replicaId ===
+   * timestamp.getSender()), it is assumed that the timestamp is
+   * causally greater than all prior messages, as described in
+   * CrdtInternal.effect, hence [] is returned.
+   */
+  getConcurrent(replicaId: string, timestamp: CausalTimestamp, arbId: number) {
+    return this.processTimestamp(replicaId, timestamp, true, arbId);
+  }
+
+  /**
+   * Performs specified actions on all messages in the history:
+   * - if returnConcurrent is true, returns the list of
+   * all messages in the history concurrent to timestamp, in
+   * receipt order.
+   * - if discardDominated is true, deletes all messages from
+   * the history whose timestamps are causally dominated by
+   * or equal to the given timestamp.  (Note that this means that
+   * if we want to keep a message with the given timestamp in
+   * the history, it must be added to the history after calling
+   * this method.)
+   */
+  private processTimestamp(
+    replicaId: string,
+    timestamp: CausalTimestamp,
+    returnConcurrent: boolean,
+    arbId: number
+  ) {
+    if (replicaId === timestamp.getSender()) {
+      return [];
+    }
+    // Gather up the concurrent messages.  These are all
+    // messages by each replicaId with sender counter
+    // greater than timestamp.asVectorClock().get(replicaId).
+    let concurrent: Array<StoredMessage> = [];
+    let vc = timestamp.asVectorClock();
+    for (let historyEntry of this.history.entries()) {
+      let senderHistory = historyEntry[1];
+      let vcEntry = vc.get(historyEntry[0]);
+      if (vcEntry === undefined) vcEntry = -1;
+      if (senderHistory !== undefined) {
+        let concurrentIndexStart = MultipleSemidirectStateBase.indexAfter(
+          senderHistory,
+          vcEntry
+        );
+        if (returnConcurrent) {
+          for (let i = concurrentIndexStart; i < senderHistory.length; i++) {
+            // With multiple semidirect products, we only consider messages
+            // from CRDTs with a higher arbitration index
+            if (senderHistory[i].arbIndex > arbId) {
+              concurrent.push(senderHistory[i]);
+            }
+          }
+        }
+      }
+    }
+    if (returnConcurrent) {
+      // Sort the concurrent messages in receipt order.
+      concurrent.sort((a, b) => a.receiptCounter - b.receiptCounter);
+      // Strip away everything except the messages.
+      return concurrent;
+    } else return [];
+  }
+
+  /**
+   * Returns true if there are no messages stored in the history,
+   * i.e., either there have been no crd1 messages, or
+   * our SemidirectInternal's historyKeepOnlyConcurrent flag is true
+   * and all crdt1 messages have been causally less than a crdt2
+   * message.
+   */
+  isHistoryEmpty(): boolean {
+    for (let value of this.history.values()) {
+      if (value.length !== 0) return false;
+    }
+    return true;
+  }
+
+  // Binary search to find first index in array with senderCounter
+  // greater than given value
+  private static binSearch(
+    arr: Array<StoredMessage>,
+    value: number,
+    start: number,
+    end: number
+  ): number {
+    if (start >= end) return start;
+
+    let mid = Math.floor((start + end) / 2);
+
+    if (arr[mid].senderCounter > value) {
+      return this.binSearch(arr, value, start, mid - 1);
+    } else {
+      return this.binSearch(arr, value, mid + 1, end);
+    }
+  }
+
+  private static indexAfter(
+    sparseArray: Array<StoredMessage>,
+    value: number
+  ): number {
+    // binary search when sparseArray is large
+    if (sparseArray.length > 100) {
+      return this.binSearch(sparseArray, value, 0, sparseArray.length);
+    }
+
+    // Note that there may be duplicate timestamps.
+    // So it would be inappropriate to find an entry whose
+    // per-sender counter equals value and infer that
+    // the desired index is 1 greater.
+    for (let i = 0; i < sparseArray.length; i++) {
+      if (sparseArray[i].senderCounter > value) return i;
+    }
+    return sparseArray.length;
+  }
+}
+
+class MultipleSemidirectStateLocallyResettable<S extends LocallyResettableState>
+  extends MultipleSemidirectStateBase<S>
+  implements LocallyResettableState
+{
+  resetLocalState() {
+    this.receiptCounter = 0;
+    this.history.clear();
+    this.internalState.resetLocalState();
+  }
+}
+
+// TODO: instead of subclass, have interface for all-but-reset part of
+// SemidirectState, then have just one class including reset?
+export type MultipleSemidirectState<S> = S extends LocallyResettableState
+  ? MultipleSemidirectStateBase<S> & LocallyResettableState
+  : MultipleSemidirectStateBase<S>;
+
+export abstract class MultipleSemidirectProduct<
+    S extends Object,
+    Events extends CrdtEventsRecord = CrdtEventsRecord
+  >
+  extends Crdt<Events>
+  implements StatefulCrdt<MultipleSemidirectState<S>>, CrdtParent
+{
+  readonly state: MultipleSemidirectState<S>;
+
+  /**
+   * TODO
+   * @param historyTimestamps=false        [description]
+   */
+  constructor(historyTimestamps = false) {
+    super();
+    // Types are hacked a bit here to make implementation simpler
+    this.state = new MultipleSemidirectStateLocallyResettable<
+      S & LocallyResettableState
+    >(historyTimestamps) as MultipleSemidirectState<S>;
+  }
+
+  protected crdts!: Array<StatefulCrdt<S>>;
+
+  /**
+   * TODO
+   * @param  m2TargetPath [description]
+   * @param  m2Timestamp  [description]
+   * @param  m2Message    [description]
+   * @param  m1TargetPath [description]
+   * @param  m1Timestamp  [description]
+   * @param  m1Message    [description]
+   * @return              [description]
+   */
+  protected abstract action(
+    m2TargetPath: string[],
+    m2Timestamp: CausalTimestamp | null,
+    m2Message: Uint8Array,
+    m1TargetPath: string[],
+    m1Timestamp: CausalTimestamp,
+    m1Message: Uint8Array
+  ): { m1TargetPath: string[]; m1Message: Uint8Array } | null;
+
+  protected setup(crdts: Array<StatefulCrdt<S>>, initialState: S) {
+    this.state.internalState = initialState;
+    this.crdts = crdts;
+    crdts.forEach((crdt) => {
+      // @ts-ignore Ignore readonly
+      crdt.state = initialState;
+    });
+
+    if (this.afterInit) this.initChildren();
+  }
+
+  init(name: string, parent: CrdtParent) {
+    super.init(name, parent);
+    if (this.crdts[0] !== undefined) {
+      this.initChildren();
+    }
+  }
+
+  private initChildren() {
+    for (let i = 0; i < this.crdts.length; i++) {
+      let crdt = this.crdts[i];
+      this.childBeingAdded = crdt;
+      crdt.init("crdt" + i.toString(), this);
+    }
+  }
+
+  private childBeingAdded?: Crdt;
+  onChildInit(child: Crdt) {
+    if (child != this.childBeingAdded) {
+      throw new Error(
+        "this was passed to Crdt.init as parent externally" +
+          " (use this.setup instead)"
+      );
+    }
+  }
+
+  protected receiveInternal(
+    targetPath: string[],
+    timestamp: CausalTimestamp,
+    message: Uint8Array
+  ) {
+    if (targetPath.length === 0) {
+      throw new Error("TODO");
+    }
+
+    let idx: number;
+    if (
+      targetPath[0].substr(0, 4) == "crdt" &&
+      (idx = parseInt(targetPath[0].substr(4))) !== NaN
+    ) {
+      let crdt = this.crdts[idx];
+      let concurrent = this.state.getConcurrent(
+        this.runtime.replicaId,
+        timestamp,
+        idx
+      );
+
+      let mAct = {
+        m1TargetPath: targetPath,
+        m1Message: message,
+      };
+      for (let i = 0; i < concurrent.length; i++) {
+        let mActOrNull = this.action(
+          concurrent[i].targetPath,
+          concurrent[i].timestamp,
+          concurrent[i].message,
+          mAct.m1TargetPath,
+          timestamp,
+          mAct.m1Message
+        );
+
+        if (mActOrNull == null) return;
+        else mAct = mActOrNull;
+      }
+
+      crdt.receive(mAct.m1TargetPath, timestamp, mAct.m1Message);
+    } else {
+      throw new Error(
+        "Unknown SemidirectProduct child: " +
+          targetPath[targetPath.length - 1] +
+          " in: " +
+          JSON.stringify(targetPath)
+      );
+    }
+
+    targetPath.length--;
+  }
+
+  getDescendant(targetPath: string[]): Crdt {
+    if (targetPath.length === 0) return this;
+
+    let idx: number;
+    let child: Crdt;
+    if (
+      targetPath[0].substr(0, 4) == "crdt" &&
+      (idx = parseInt(targetPath[0].substr(4))) !== NaN
+    ) {
+      let idx = parseInt(targetPath[0].substr(4));
+      child = this.crdts[idx];
+    } else {
+      throw new Error(
+        "Unknown child: " +
+          targetPath[targetPath.length - 1] +
+          " in SemidirectProduct: " +
+          JSON.stringify(targetPath)
+      );
+    }
+
+    targetPath.length--;
+    return child.getDescendant(targetPath);
+  }
+
+  canGc(): boolean {
+    // TODO: this may spuriously return false if one of the Crdt's is not
+    // in its initial state only because we overwrote that state with
+    // the semidirect initial state.  Although, for our Crdt's so far
+    // (e.g NumberCrdt), it ends up working because they check canGC()
+    // by asking the state if it is in its initial state.
+    let crdtsCanGc = true;
+    this.crdts.forEach((crdt) => {
+      crdtsCanGc = crdtsCanGc && crdt.canGc();
+    });
+
+    return this.state.isHistoryEmpty() && crdtsCanGc;
+  }
+}
