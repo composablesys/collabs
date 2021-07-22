704c60a8e1a3d114fa7339c47a9c14b5e8168a4a
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2021-07-22 16:12:55 -0400

    add setExisting to MutCRegister

--- client/src/crdt/register/mut_register.ts
+++ client/src/crdt/register/mut_register.ts
@@ -16,7 +16,7 @@ import { FwwCRegister, LwwCRegister } from "./wins_registers";
 
 export class MutCRegister<C extends Crdt, SetArgs extends any[]>
   extends CompositeCrdt<ResettableEventsRecord>
-  implements CRegister<Optional<C>, SetArgs>, Resettable
+  implements CRegister<Optional<C>, SetArgs>, Resettable 
 {
   private readonly crdtFactory: DeletingMutCSet<C, SetArgs>;
   private readonly register: FwwCRegister<C> | LwwCRegister<C>;
@@ -73,6 +73,67 @@ export class MutCRegister<C extends Crdt, SetArgs extends any[]>
 
   conflictsMeta(): AggregateCRegisterMeta<C>[] {
     return this.register.conflictsMeta();
+  }
+
+  owns(value: C): boolean {
+    return this.crdtFactory.owns(value);
+  }
+
+  /**
+   * Returns whether the given value is owned by
+   * this register and still usable, i.e.,
+   * it can still accept Crdt operations.
+   * Values are made unusable once they are overwritten
+   * by a causally later call to set or reset (but not
+   * setExisting).
+   *
+   * isUsable is always true for the set value and
+   * elements of conflicts().  It is true for value
+   * not in conflicts() only if value has been
+   * overwritten by setExisting but not by set or reset.
+   */
+  isUsable(value: C): boolean {
+    return this.crdtFactory.has(value);
+  }
+
+  /**
+   * Set the value to a previous value that isUsable
+   * (e.g., an element of conflicts()).
+   *
+   * A typical use case is if multiple users set the
+   * value concurrently and each do some work on their
+   * value, causing a conflict, they can inspect the conflicts
+   * (using conflicts()) and choose one to appear as
+   * the main value.  In case multiple users do so
+   * concurrently, one of their choices will be chosen
+   * using the usual conflict resolution rule (LWW or FWW)
+   * for this register.
+   *
+   * Unlike set, this method does not delete (make unusable)
+   * other existing elements; instead, they will be still
+   * be usable as standalone CRDTs, and they can be
+   * passed to a future or concurrent call to setExisting.
+   * That is so that if two users concurrently call
+   * setExisting on different conflicting values, the
+   * winning value will still be usable as a Crdt.
+   * Existing values will instead be deleted on the next
+   * call to set or reset, in which case they will
+   * be deleted and made unusable even if they no
+   * longer appear in conflicts().
+   *
+   * @param  value [description]
+   * @return       [description]
+   * @throws if value is not owned by this register or
+   * it has been deleted (made unusable), due to a causally
+   * greater call to delete or set (but not setExisting).
+   */
+  setExisting(value: C) {
+    if (!this.isUsable(value)) {
+      throw new Error("value has already been deleted or is not owned by us");
+    }
+    // TypeScript doesn't understand that value is of type
+    // C, not an arbitrary T, due to the union type
+    (this.register.value as C) = value;
   }
 
   reset() {
