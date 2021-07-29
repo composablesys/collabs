62afc4ec08f7fe4d060bc6326f1c177acd5df31e
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2021-07-29 15:48:28 -0400

    add copyright notice for reused TypeScript annotations

--- client/src/crdt/set/interfaces.ts
+++ client/src/crdt/set/interfaces.ts
@@ -1,3 +1,27 @@
+/**
+ * Some methods in CSet are copied or modified from methods on
+ * JavaScript's Set class, in which case their
+ * type annotations and docstrings are copied or modified from
+ * those used in TypeScript, found in various files in
+ * https://github.com/microsoft/TypeScript/tree/main/src/lib
+ * Material from TypeScript
+ * bears the following copyright notice:
+ *
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+
 import { Crdt, CrdtEvent, CrdtEventsRecord } from "../core";
 
 export interface CSetEvent<T> extends CrdtEvent {
