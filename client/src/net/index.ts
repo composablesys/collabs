054a4cd30aeaf3a03495ad6fcc884deea1d82f5d
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2021-08-11 11:50:35 -0400

    rearrange main package files (previously client)

--- client/src/net/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./causal_broadcast_network";
-export * from "./web_rtc_network";
-export * from "./default_causal_broadcast_network";
-export * from "./vector_clock";
-export * from "./web_socket_network";
-export * from "./testing_network";
-export * from "./matrix_widget_network";
