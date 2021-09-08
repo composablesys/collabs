c1ac0910fc9ddd974926f02feba09c97874b9942
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2021-09-08 17:57:10 -0400

    skip unimplemented tensor save tests

--- benchmarks/micro_crdts.sh
+++ benchmarks/micro_crdts.sh
@@ -18,7 +18,12 @@ do
     do
       for name in "NoopCrdt" "DeepNoopCrdt" "Counter" "Counter-1" "Counter-10" "Counter-50" "Counter-100" "MultiValueRegister" "Register" "NumberCrdt" "EnableWinsFlag" "AddWinsSet" "AddWinsSetRolling" "AddWinsSetRollingGrow" "MapCrdt" "MapCrdtRolling" "MapCrdtRollingGrow" "LwwMap" "LwwMapRolling" "TextLtr" "TextRandom" "LwwMapRollingGrow" "TextLtrGrow" "TextRandomGrow" "RgaLtr" "RgaRandom" "RgaLtrGrow" "RgaRandomGrow" "TensorAvg-10" "TensorCounter-10"
       do
-        npm start -- $1 $2 $3 $4 "micro_crdts" $name $measurement $frequency
+        if [ $measurement == "save" ] && [[ $name == Tensor* ]]
+        then
+          echo "Skipping micro_crdts $name save (not implemented)"
+        else
+          npm start -- $1 $2 $3 $4 "micro_crdts" $name $measurement $frequency
+        fi
       done
     done
 done
