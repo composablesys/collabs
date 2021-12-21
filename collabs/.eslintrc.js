6f35e851c2caa6cc394b89e759a8582237ed657e
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2021-12-20 20:53:45 -0500

    make linter play nice with Atom

--- collabs/.eslintrc.js
+++ collabs/.eslintrc.js
@@ -19,4 +19,20 @@ module.exports = {
     "plugin:import/typescript",
     "prettier",
   ],
+  rules: {
+    // Allow inference in function return type.
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    // Remove "object" from ban-types banned list.
+    // If eslint v8 worked with Atom, we could just upgrade
+    // to that, which already unbans it.
+    "@typescript-eslint/ban-types": [
+      "error",
+      {
+        types: {
+          object: false,
+        },
+      },
+    ],
+  },
 };
