4f5a1498e51d8180048c55a05878da2556772cca
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2022-04-02 13:04:37 -0400

    YjsReplica fixes

--- benchmarks/src/replica-benchmark/implementations/yjs/replica.ts
+++ benchmarks/src/replica-benchmark/implementations/yjs/replica.ts
@@ -4,8 +4,6 @@ import { Replica } from "../../replica_benchmark";
 
 export class YjsReplica implements Replica {
   readonly doc: Y.Doc;
-  // TODO: find better approach
-  private inReceive = false;
 
   constructor(
     private readonly onsend: (msg: Data) => void,
@@ -14,26 +12,25 @@ export class YjsReplica implements Replica {
     this.doc = new Y.Doc({ guid: uuidv4(replicaIdRng) });
     // clientID is normally set with lib0/random.uint32(), so we
     // need to get a PRNG uint32 instead.
-    this.doc.clientID = replicaIdRng.int32() + (2 << 31);
-    this.doc.on("update", (update: Uint8Array) => {
+    this.doc.clientID = replicaIdRng.int32() + Math.pow(2, 31);
+    this.doc.on("update", (update: Uint8Array, origin: boolean) => {
       // Yjs dispatches update events not just when this sends
       // a message, but also when it receives one for
-      // the first time.  We only care when they actually
-      // send the message.
-      if (!this.inReceive) {
+      // the first time.  We only care when this.doc actually
+      // sends the message. To check this, we use origin "true" in
+      // this.transact() and "false" otherwise.
+      if (origin) {
         this.onsend(update);
       }
     });
   }
 
   transact(doOps: () => void) {
-    this.doc.transact(doOps);
+    this.doc.transact(doOps, true);
   }
 
   receive(msg: Uint8Array): void {
-    this.inReceive = true;
-    Y.applyUpdate(this.doc, msg);
-    this.inReceive = false;
+    Y.applyUpdate(this.doc, msg, false);
   }
 
   save(): Data {
@@ -41,9 +38,7 @@ export class YjsReplica implements Replica {
   }
 
   load(saveData: Uint8Array): void {
-    this.inReceive = true;
-    Y.applyUpdate(this.doc, saveData);
-    this.inReceive = false;
+    Y.applyUpdate(this.doc, saveData, false);
   }
 
   skipLoad(): void {}
