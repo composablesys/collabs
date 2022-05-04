bc7f664b6de935e8bc3b03fcdd73512e6a53895d
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2022-05-04 15:05:22 -0400

    docs build all packages

--- docs/typedoc.sh
+++ docs/typedoc.sh
@@ -6,8 +6,7 @@ set -x
 cd ..
 
 # TODO: dedupe package list. Must be in build order.
-# Only include packages that need to built for some other package.
-for package in "core" "crdts" "collabs"
+for package in "core" "crdts" "collabs" "container" "matrix-widget" "ws-client" "ws-server"
 do
   npm run build -w $package
 done
@@ -15,7 +14,7 @@ done
 cd docs
 
 # Only include published packages with TypeScript APIs (the ones you want public API docs for).
-for package in "collabs" "container" "core" "crdts" "matrix-widget" "ws-client" "ws-server"
+for package in "core" "crdts" "collabs" "container" "matrix-widget" "ws-client" "ws-server"
 do
   typedoc --out generated/api/$package ../$package/src/index.ts --tsconfig ../$package/tsconfig.json
 done
