face5b4a28cd402c9a9c69bb8bb571197a57fb4a
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2024-05-01 18:09:43 -0400

    try old setup with sphinx 5

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -60,4 +60,6 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_extra_path = ["../generated"]
+
 html_favicon = 'favicon.ico'
